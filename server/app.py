a4ef3236d518c709832ee22275408f47fefffee9
Ruben Salazar <rubensalazar@rocketmail.com>
2025-03-25 18:52:49 -0500

    chatterBox

--- server/app.py
+++ server/app.py
@@ -14,13 +14,70 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages')
+def get_all_messages():
+    
+    messages = [message.to_dict() for message in Message.query.all()]
+   
+    return messages, 200
+
+
+@app.route('/messages', methods=['POST'])
+def create():
+    data = request.get_json()
+
+    new_message = Message(
+        body=data['body'],
+        username=data['username']
+    )
+
+    db.session.add(new_message)
+    db.session.commit()
+
+    return make_response(new_message.to_dict(), 201)
+
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
+def message_by_id(id):
+    
+    
+    if request.method == "PATCH":
+        data = request.get_json()
+        message = db.session.get(Message, id)
+        
+        message.body = data['body']
+
+        db.session.commit()
+
+        return make_response(message.to_dict(), 201)
+    
+
+    elif request.method == "DELETE":
+        
+        message = db.session.get(Message, id)
+        db.session.delete(message)
+        db.session.commit()
+
+        return {'message': 'Passage deleted!'}, 200
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+   
+
+
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,14 +1,30 @@
+from datetime import datetime
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
-
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(metadata=metadata)
+
+
+CORS(app)
 
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    serialize_only = ('id', 'body', 'username', 'created_at', 'updated_at')
+
+    
